14eceb1a2bd47b234e889cfb87af76d1f1469b60
Saiminator <42367509+Saiminator@users.noreply.github.com>
2025-03-27 03:07:47 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,6 +247,7 @@ document.addEventListener('DOMContentLoaded', function() {
     "WRATH": "wrath",
     "SHIHO": "shiho",
     "EXTREME": "extreme"
+    "TESTING": "testing"
   };
   
   const MASTER_SECRET = "ALLSECRETS";
